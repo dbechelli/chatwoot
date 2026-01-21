a2c2b21436f6d6338bfcc7bcdd130a12eb8ebf05
Visual Software Technology <denisbechellic@gmail.com>
2026-01-20 23:23:15 -0300

    FIX OUTHJER

--- app/javascript/dashboard/routes/dashboard/settings/kanban/KanbanTemplates.vue
+++ app/javascript/dashboard/routes/dashboard/settings/kanban/KanbanTemplates.vue
@@ -218,6 +218,7 @@ const selectTemplate = (template) => {
 .line-clamp-2 {
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
